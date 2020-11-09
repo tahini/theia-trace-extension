61aeb5ffb208edf8f6a37eb3d183aae63ff627f7
Geneviève Bastien <gbastien@versatic.net>
2020-11-09 13:46:49 -0500

    Split viewer-prototype in packages
    In prevision of the development of a vscode extension, the code in the
    viewer-prototype has been mostly moved to 2 new packages:
    
    * @trace-viewer/base: Queries the trace server for traces, experiments
    and possible outputs
    * @trace-viewer/react-components: Contains the various types of
    visualization for trace data, queries the trace server for output data
    to display
    
    Those 2 packages can eventually be published to npm.
    
    In the viewer-prototype directory only remains the theia specific module
    contributions. So the application still works as before using theia,
    it's just that most of the classes used by the trace-viewer have been
    moved to new npm packages.
    
    Signed-off-by: Geneviève Bastien <gbastien@versatic.net>

---
+++ packages/base/src/signal-manager.ts
@@ -0,0 +1,31 @@
+import { EventEmitter } from 'events';
+
+export declare interface SignalManager {
+
+    fireTooltipSignal(payload: { [key: string]: string }): void;
+
+}
+
+export const Signals = {
+    TRACE_OPENED : 'trace opened',
+    TRACE_CLOSED : 'trace closed',
+    EXPERIMENT_OPENED: 'experiment opened',
+    EXPERIMENT_CLOSED: 'experiment closed',
+    EXPERIMENT_SELECTED: 'experiment selected'
+};
+
+export class SignalManager extends EventEmitter implements SignalManager {
+
+    fireTooltipSignal(payload: { [key: string]: string; }): void {
+        /* To be implemented by extending clases */
+    }
+
+}
+
+let instance: SignalManager = new SignalManager();
+
+export const setSignalManagerInstance = (sm: SignalManager) => {
+    instance = sm;
+};
+
+export const signalManager = (): SignalManager => instance;
